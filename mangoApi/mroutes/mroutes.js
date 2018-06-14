0ed2d4b641841c07576c9420a037096349bcc2f6
Bhupender <bskadian123@gmail.com>
2018-06-14 11:47:04 +0530

    Signed-off-by: Bhupender <bskadian123@gmail.com>

--- mangoApi/mroutes/mroutes.js
+++ mangoApi/mroutes/mroutes.js
@@ -12,4 +12,24 @@ route.get('/companies/:name',function(request,response){
     });
 });
 
+route.get("/cname/:pattern",function(request,response){
+companies.find({name:{$regex:request.params.pattern,$options:'i'}},{_id:0,name:1},
+function(err,data){
+    if(err){
+    response.send(err);
+    response.json([]);
+    }
+    response.json(data);
+})
+});
+
+//update the employee
+route.put("/empcount/:name",function(request,response){
+    let cname=request.params.name;
+    companies.update({name:cname},{$set:request.body},function(err,data){
+        if(err)
+        response.send({result:"not updated"});
+        response.send({result:"Successfully updated"});
+    });
+});
 module.exports=route;
